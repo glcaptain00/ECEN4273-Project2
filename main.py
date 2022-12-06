8c324963001f5836b3e47f77f48e7325899e5d14
glcaptain00 <60938173+glcaptain00@users.noreply.github.com>
2022-12-06 10:23:27 -0600

    Added handling source type

--- main.py
+++ main.py
@@ -9,6 +9,9 @@ import traceback    #For printing excpetion trace
 ############################
 # Video Processing Section #
 ############################
+def openImage(src):
+    return cv.imread(src)
+
 def openVideo(video):
     return cv.VideoCapture(video)
 
@@ -33,15 +36,33 @@ def splitFrames(video):
 # Main #
 ########
 
-if (len(sys.argv) < 2):
-    print("Usage: python main.py <video source>")
+if (len(sys.argv) < 3):
+    print("Usage: python main.py <video source type> <file name>\nWhere type can be:\n\t-live: file name will be an integer representing the camera to stream video from\n\t-img: Use an image file\n\t-video: Use a video file")
     sys.exit()
 
-videoSource = sys.argv[1]
+sourceType = sys.argv[1]
+videoSource = sys.argv[2]
 
-myVid = openVideo(videoSource)
-myFrames = splitFrames(myVid)
+if (sourceType == "-live"):
+    print("Live not yet implemented")
+    sys.exit()
+elif (sourceType == "-img" or videoSource == "-image"):
+    im = cv.imread(videoSource)
 
-cv.imshow("Frame", myFrames[0])
-cv.waitKey(0)
+    newIm = cv.pyrDown(im)
+    ret, newIm = cv.threshold(cv.cvtColor(newIm, cv.COLOR_BGR2GRAY), 64, 255, cv.THRESH_BINARY)
+    cv.imshow("Frame", newIm)
+    cv.waitKey(0)
+
+elif (sourceType == "-video"):
+
+    myVid = openVideo(videoSource)
+    myFrames = splitFrames(myVid)
+
+    #cv.rectangle(myFrames[0], (10, 10), (30,30), (255,0,0), 2)
+
+    newImg = cv.pyrDown(myFrames[0])
+    ret, newImg = cv.threshold(cv.cvtColor(newImg, cv.COLOR_BGR2GRAY), 64, 255, cv.THRESH_BINARY)
+    cv.imshow("Frame", newImg)
+    cv.waitKey(0)
 
